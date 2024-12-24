04ee452510a96ce29c8054a1d62a65f50986a117
anasamin22311 <anasamin22311@gmail.com>
2024-12-24 19:20:04 +0200

    d

--- Presentation/Controllers/PriceRequestsController.cs
+++ Presentation/Controllers/PriceRequestsController.cs
@@ -17,43 +17,43 @@ namespace Presentation.Controllers
         }
 
         [HttpPost("createRequest")]
-        [HasPermission(Permissions.CreatePriceRequest)]
+        //[HasPermission(Permissions.CreatePriceRequest)]
         public async Task<IActionResult> CreateRequestAsync(CreatePriceRequestDto dto)
             => Result(await _priceRequestsService.CreateRequestAsync(dto));
 
         [HttpGet("getRequestForFacility")]
         [HasAccountType(AccountTypes.Facility)]
-        [HasPermission(Permissions.ViewPriceRequests)]
+        //[HasPermission(Permissions.ViewPriceRequests)]
         public async Task<IActionResult> GetRequestForFacilityAsync()
             => Result(await _priceRequestsService.GetRequestForFacilityAsync());
 
         [HttpGet("getRequestForCompany")]
         [HasAccountType(AccountTypes.Company)]
-        [HasPermission(Permissions.ViewPriceRequests)]
+        //[HasPermission(Permissions.ViewPriceRequests)]
         public async Task<IActionResult> GetRequestForCompanyAsync()
             => Result(await _priceRequestsService.GetRequestForCompanyAsync());
         
         [HttpPost("createFacilityDetails")]
-        [HasPermission(Permissions.CreatePriceRequest)]
+        //[HasPermission(Permissions.CreatePriceRequest)]
         public async Task<IActionResult> CreateFacilityDetailsAsync(CreatePriceRequestFacilityDetailsDto dto)
             => Result(await _priceRequestsService.CreateFacilityDetailsAsync(dto));
         [HttpPut("updateFacilityDetails")]
-        [HasPermission(Permissions.UpdatePriceRequest)]
+        //[HasPermission(Permissions.UpdatePriceRequest)]
         public async Task<IActionResult> UpdateFacilityDetailsAsync(long facilityDetailsId, UpdatePriceRequestFacilityDetailsDto dto)
             => Result(await _priceRequestsService.UpdateFacilityDetailsAsync(facilityDetailsId, dto));
 
         [HttpGet("getFacilityDetails")]
-        [HasPermission(Permissions.ViewPriceRequests)]
+        //[HasPermission(Permissions.ViewPriceRequests)]
         public async Task<IActionResult> GetFacilityDetailsAsync(long priceRequestId)
             => Result(await _priceRequestsService.GetFacilityDetailsAsync(priceRequestId));
 
         [HttpPatch("acceptRequest")]
-        [HasPermission(Permissions.AcceptPriceRequest)]
+        //[HasPermission(Permissions.AcceptPriceRequest)]
         public async Task<IActionResult> AcceptRequestAsync(CreatePriceRequestResponseDto dto)
            => Result(await _priceRequestsService.AcceptRequestAsync(dto));
 
         [HttpPatch("rejectRequest")]
-        [HasPermission(Permissions.RejectPriceRequest)]
+        //[HasPermission(Permissions.RejectPriceRequest)]
         public async Task<IActionResult> RejectRequestAsync(long priceRequestId)
             => Result(await _priceRequestsService.RejectRequestAsync(priceRequestId));
     }
