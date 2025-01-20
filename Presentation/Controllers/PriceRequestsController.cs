0ab60956840fd009ecc252d5664814925acee8f4
anasamin22311 <anasamin22311@gmail.com>
2025-01-20 12:05:16 +0200

    Price request

--- Presentation/Controllers/PriceRequestsController.cs
+++ Presentation/Controllers/PriceRequestsController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Presentation.Controllers
 {
     [Route("api/priceRequests")]
-    [Authorize]
+    //[Authorize]
     public class PriceRequestsController : ApiControllerBase
     {
         private readonly IPriceRequestsService _priceRequestsService;
@@ -18,57 +18,57 @@ namespace Presentation.Controllers
         }
 
         [HttpPost("createRequest")]
-        [HasAccountType(AccountTypes.Facility)]
+        //[HasAccountType(AccountTypes.Facility)]
         public async Task<IActionResult> CreateRequestAsync(CreatePriceRequestDto dto)
             => Result(await _priceRequestsService.CreateRequestAsync(dto));
 
         [HttpPatch("acceptRequest")]
-        [HasAccountType(AccountTypes.Company)]
+        //[HasAccountType(AccountTypes.Company)]
         public async Task<IActionResult> AcceptRequestAsync(CreatePriceRequestOfferDto dto)
             => Result(await _priceRequestsService.AcceptRequestAsync(dto));
         
         [HttpPatch("rejectRequest")]
-        [HasAccountType(AccountTypes.Company)]
+        //[HasAccountType(AccountTypes.Company)]
         public async Task<IActionResult> RejectRequestAsync(long priceRequestId)
             => Result(await _priceRequestsService.RejectRequestAsync(priceRequestId));
 
         [HttpPatch("cancelRequest")]
-        [HasAccountType(AccountTypes.Facility)]
+        //[HasAccountType(AccountTypes.Facility)]
         public async Task<IActionResult> CancelRequestAsync(long priceRequestId)
             => Result(await _priceRequestsService.CancelRequestAsync(priceRequestId));
 
         [HttpGet("getMyRequestsAsFacility")]
-        [HasAccountType(AccountTypes.Facility)]
+        //[HasAccountType(AccountTypes.Facility)]
         public async Task<IActionResult> GetMyRequestsAsFacilityAsync()
             => Result(await _priceRequestsService.GetMyRequestsAsFacilityAsync());
 
         [HttpGet("getMyRequestsAsCompany")]
-        [HasAccountType(AccountTypes.Company)]
+        //[HasAccountType(AccountTypes.Company)]
         public async Task<IActionResult> GetMyRequestsAsCompanyAsync()
             => Result(await _priceRequestsService.GetMyRequestsAsCompanyAsync());
 
         [HttpPost("createRequestFacilityDetails")]
-        [HasAccountType(AccountTypes.Facility)]
+        //[HasAccountType(AccountTypes.Facility)]
         public async Task<IActionResult> CreateRequestFacilityDetailsAsync(CreatePriceRequestFacilityDetailsDto dto)
             => Result(await _priceRequestsService.CreateRequestFacilityDetailsAsync(dto));
 
         [HttpPut("updateRequestFacilityDetails")]
-        [HasAccountType(AccountTypes.Facility)]
+        //[HasAccountType(AccountTypes.Facility)]
         public async Task<IActionResult> UpdateRequestFacilityDetailsAsync(long facilityDetailsId, UpdatePriceRequestFacilityDetailsDto dto)
             => Result(await _priceRequestsService.UpdateRequestFacilityDetailsAsync(facilityDetailsId, dto));
 
         [HttpGet("getRequestFacilityDetails")]
-        [HaveAccountTypes(AccountTypes.Facility, AccountTypes.Company)]
+        //[HaveAccountTypes(AccountTypes.Facility, AccountTypes.Company)]
         public async Task<IActionResult> GetRequestFacilityDetailsAsync(long priceRequestId)
             => Result(await _priceRequestsService.GetRequestFacilityDetailsAsync(priceRequestId));
         
         [HttpPost("createRequestMessage")]
-        [HaveAccountTypes(AccountTypes.Facility, AccountTypes.Company)]
+        //[HaveAccountTypes(AccountTypes.Facility, AccountTypes.Company)]
         public async Task<IActionResult> CreateRequestMessageAsync(CreatePriceRequestMessageDto dto)
             => Result(await _priceRequestsService.CreateRequestMessageAsync(dto));
         
         [HttpPost("getRequestMessages")]
-        [HaveAccountTypes(AccountTypes.Facility, AccountTypes.Company)]
+        //[HaveAccountTypes(AccountTypes.Facility, AccountTypes.Company)]
         public async Task<IActionResult> GetRequestMessagesAsync(long requestId)
             => Result(await _priceRequestsService.GetRequestMessagesAsync(requestId));
     }
