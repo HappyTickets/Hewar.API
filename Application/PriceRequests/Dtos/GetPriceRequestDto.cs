46acc552a8e1e11688f2b7174b122fadf2c279c7
Mohamed Ahmed <mo.19.ahmed.47@gmail.com>
2025-02-09 09:00:50 +0200

    Additional Fields Get PR By Id Dto

--- Application/PriceRequests/Dtos/GetPriceRequestDto.cs
+++ Application/PriceRequests/Dtos/GetPriceRequestDto.cs
@@ -1,18 +1,22 @@
-﻿namespace Application.PriceRequests.Dtos
+﻿using Application.Companies.Dtos;
+using Application.Facilities.Dtos;
+using Application.PriceRequests.Dtos.Services;
+
+namespace Application.PriceRequests.Dtos
 {
     public class GetPriceRequestDto
     {
         public long Id { get; set; }
         public long? ChatId { get; set; }
-        public long CompanyId { get; set; }
-        public long FacilityId { get; set; }
-
         public ContractType ContractType { get; set; }
         public DateTimeOffset StartDate { get; set; }
         public DateTimeOffset EndDate { get; set; }
         public RequestStatus Status { get; set; }
-        public string FacilityName { get; set; }
-        public string CompanyName { get; set; }
+        public ICollection<ServiceRequestDto>? Services { get; set; }
+        public ICollection<OtherRequestedServiceDto>? OtherServices { get; set; }
+        public FacilityBreifDto Facility { get; set; }
+        public CompanyBreifDto Company { get; set; }
+
         public string? Notes { get; set; }
         public bool HasOffers { get; set; }
     }
