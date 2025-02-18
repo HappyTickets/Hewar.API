919a984227d9439ac0419b863ea7f2bd16535bc6
Mohamed Ahmed <mo.19.ahmed.47@gmail.com>
2025-02-18 06:22:25 +0200

    remove json files

--- Infrastructure/Persistence/Seeds/StaticContractSeeder.cs
+++ Infrastructure/Persistence/Seeds/StaticContractSeeder.cs
@@ -1,5 +1,4 @@
-﻿using Domain.Entities.ContractAggregate.Static;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Text.Json;
 
 namespace Infrastructure.Persistence.Seeds.Contract
@@ -9,12 +8,12 @@ namespace Infrastructure.Persistence.Seeds.Contract
         public static ModelBuilder SeedStaticContract(this ModelBuilder builder)
         {
             // Seed StaticContract data
-            var staticContractData = ReadJsonData<StaticContract>("StaticContractData.json");
-            builder.Entity<StaticContract>().HasData(staticContractData);
+            // ,//var staticContractData = ReadJsonData<StaticContract>("StaticContractData.json");
+            //   builder.Entity<StaticContract>().HasData(staticContractData);
 
             // Seed StaticClause data
-            var staticClausesData = ReadJsonData<StaticClause[]>("StaticClauses.json");
-            builder.Entity<StaticClause>().HasData(staticClausesData);
+            //   var staticClausesData = ReadJsonData<StaticClause[]>("StaticClauses.json");
+            //     builder.Entity<StaticClause>().HasData(staticClausesData);
 
             return builder;
         }
